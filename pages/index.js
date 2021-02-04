2418dfe453dcde193e41101856cb4e25cf700354
Ike Oche <ikeemma.emmanuel@gmail.com>
2021-02-04 23:03:32 +0100

    color

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default function Home() {
         </div>
           
           <div>
-              <nav className="text-blue-500 font-semibold flex flex-row w-1/2">
+              <nav className="font-semibold flex flex-row w-1/2">
                   <p className="m-5 w-50">Work</p>
                   <p className="m-5 w-50">Products</p>
                   <p className="m-5 w-50">Pricing</p>
@@ -30,11 +30,11 @@ export default function Home() {
 
         <div className="flex w-max flex-row h-screen">
             <div className="flex-1 m-5 p-10">
-                <h1 className=" mb-5 mt-20 text-5xl text-blue-500 font-bold">Build Impactful and scaleable products </h1>
-                <p className="mb-5 text-1xl">Intience is a user centric digital agency that helps individuals, businesses and governments build impactful digital products</p>
-                <div>
-                    <button className="ml-0 m-1 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Talk to Us</button>
-                    <button className="m-1 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">About Us</button>
+                <h1 className="ml-20 mb-5 mt-20 text-5xl font-bold">Build Impactful and scaleable products </h1>
+                <p className="ml-20 mb-5 text-1xl">Intience is a user centric digital agency that helps individuals, businesses and governments build impactful digital products</p>
+                <div className="m-20 mt-10">
+                    <button className="ml-0 m-1 bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-6 rounded">Talk to Us</button>
+                    <button className="m-1 bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-6 rounded">About Us</button>
                 </div>
             </div>
             <div className="flex-1">
@@ -45,23 +45,23 @@ export default function Home() {
         {/* About Us */}
         <div className="h-screen w-full">
             <div className="w-full flex justify-center flex-column">
-                <h1 className="text-5xl text-blue-500 font-bold text-center">What we do</h1>
+                <h1 className="text-5xl text-black font-bold text-center">What we do</h1>
             </div>
             <div className="w-full flex flex-row flex-wrap p-20 justify-center">
-                <div className="m-5 p-5 bg-blue flex flex-col content-center bg-blue-100 h-64 w-64 text-center">
-                    <h3 className="mb-5 text-2xl font-semibold">Mobile Development</h3>
+                <div className="m-5 p-5 bg-blue flex flex-col content-center h-64 w-64 text-center">
+                    <h3 className="mb-5 text-2xl text-blue-500 font-semibold">Mobile Development</h3>
                     <p>We utilize cutting edge technology to build world class android and ios apps</p>
                 </div>
-               <div className="m-5 p-5 bg-blue flex flex-col content-center bg-blue-100 h-64 w-64 text-center">
-                    <h3 className="mb-5 text-2xl font-semibold">Mobile Development</h3>
+               <div className="m-5 p-5 bg-blue flex flex-col content-center h-64 w-64 text-center">
+                    <h3 className="mb-5 text-2xl text-blue-500 font-semibold">Mobile Development</h3>
                     <p>We utilize cutting edge technology to build world class android and ios apps</p>
                 </div>
-                <div className="m-5 p-5 bg-blue flex flex-col content-center bg-blue-100 h-64 w-64 text-center">
-                    <h3 className="mb-5 text-2xl font-semibold">Mobile Development</h3>
+                <div className="m-5 p-5 bg-blue flex flex-col content-center h-64 w-64 text-center">
+                    <h3 className="mb-5 text-2xl text-blue-500 font-semibold">Mobile Development</h3>
                     <p>We utilize cutting edge technology to build world class android and ios apps</p>
                 </div>
-                <div className="m-5 p-5 bg-blue flex flex-col content-center bg-blue-100 h-64 w-64 text-center">
-                    <h3 className="mb-5 text-2xl font-semibold">Mobile Development</h3>
+                <div className="m-5 p-5 bg-blue flex flex-col content-center h-64 w-64 text-center">
+                    <h3 className="mb-5 text-2xl text-blue-500 font-semibold">Mobile Development</h3>
                     <p>We utilize cutting edge technology to build world class android and ios apps</p>
                 </div>
             </div>
@@ -69,10 +69,23 @@ export default function Home() {
 
         {/* Our creative process */}
         <div className="h-screen w-full">
-            <div><h1>Our Creative Process</h1></div>
-            <div>
-                <div>
-                    <h3>NodeJS</h3>
+            <div><h1 className="text-5xl text-black font-bold text-center">Our Creative Process</h1></div>
+            <div className="flex flex-row flex-wrap p-20 justify-center" >
+                <div className="m-5 p-5 bg-gray-300 flex flex-col content-center h-64 w-64 text-center">
+                    <h3 className="mb-5 text-2xl text-black font-semibold">Mobile Development</h3>
+                    <p>We utilize cutting edge technology to build world class android and ios apps</p>
+                </div>
+                <div className="m-5 p-5 bg-blue flex flex-col content-center h-64 w-64 text-center">
+                    <h3 className="mb-5 text-2xl text-blue-500 font-semibold">Mobile Development</h3>
+                    <p>We utilize cutting edge technology to build world class android and ios apps</p>
+                </div>
+                <div className="m-5 p-5 bg-blue flex flex-col content-center h-64 w-64 text-center">
+                    <h3 className="mb-5 text-2xl text-blue-500 font-semibold">Mobile Development</h3>
+                    <p>We utilize cutting edge technology to build world class android and ios apps</p>
+                </div>
+                <div className="m-5 p-5 bg-blue flex flex-col content-center h-64 w-64 text-center">
+                    <h3 className="mb-5 text-2xl text-blue-500 font-semibold">Mobile Development</h3>
+                    <p>We utilize cutting edge technology to build world class android and ios apps</p>
                 </div>
             </div>
         </div>
@@ -91,29 +104,29 @@ export default function Home() {
         </div>
       </main>
 
-      <footer  className="h-screen w-full" >
+      <footer  className="w-full" >
       <div className="w-full flex flex-row">
-        <div>
+        <div className="w-52 m-5">
           <img src="/dd.jpg" alt="logo" className="w-30 h-20"/>
         </div>
-        <div>
-            <h3>Links</h3>
+        <div className="w-52 m-5">
+            <h3 className="text-1xl font-bold">Links</h3>
             <nav>
                 <p>Services</p>
                   <p>Products</p>
                   <p>Contact Us</p>
             </nav>
         </div>
-        <div>
-            <h3>GEt In touch</h3>
+        <div className="w-52 m-5">
+            <h3 className="text-1xl font-bold">Get In touch</h3>
             <nav>
                 <p>twitter</p>
                   <p>instagram</p>
                   <p>mobile</p>
             </nav>
         </div>
-        <div>
-            <h3>Company</h3>
+        <div className="w-52 m-5">
+            <h3 className="text-1xl font-bold">Company</h3>
             <nav>
                 <p>About</p>
                   <p>Blog</p>
@@ -121,8 +134,8 @@ export default function Home() {
             </nav>
         </div>
         </div>
-        <div>
-            <p>Copywrite 2021 intience labs</p>
+        <div className="m-10 border-t-4 border-gray-200 pt-10">
+            <p className="text-1xl font-bold">Copywrite 2021 intience labs</p>
         </div>
       </footer>
     </div>
