694083ab368d260a9511318153821bb18c433e47
Ike Oche <ikeemma.emmanuel@gmail.com>
2021-02-06 17:24:27 +0100

    added new information

--- pages/index.js
+++ pages/index.js
@@ -19,22 +19,22 @@ export default function Home() {
           
           <div>
               <nav className="font-semibold flex flex-row w-1/2">
-                  <p className="m-5 w-50">Work</p>
-                  <p className="m-5 w-50">Products</p>
-                  <p className="m-5 w-50">Pricing</p>
-                  <p className="m-5 w-50">Contact Us</p>
+                  <p className="m-5 w-94">Work</p>
+                  <p className="m-5 w-94">Products</p>
+                  <p className="m-5 w-94">Pricing</p>
+                  <p className="m-5 w-94">Contact Us</p>
               </nav>
               
           </div>
         </div>
 
-        <div className="flex w-max flex-row h-screen">
+        <div className="flex w-full flex-row h-screen">
             <div className="flex-1 m-5 p-10">
                 <h1 className="ml-20 mb-5 mt-20 text-5xl font-bold">Build Impactful and scaleable products </h1>
                 <p className="ml-20 mb-5 text-1xl">Intience is a user centric digital agency that helps individuals, businesses and governments build impactful digital products</p>
                 <div className="m-20 mt-10">
-                    <button className="ml-0 m-1 bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-6 rounded">Talk to Us</button>
-                    <button className="m-1 bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-6 rounded">About Us</button>
+                    <button className="ml-0 m-1 bg-blue-800 hover:bg-blue-800 text-white font-bold py-4 px-6 rounded">Talk to Us</button>
+                    <button className="m-1 bg-blue-800 hover:bg-blue-800 text-white font-bold py-4 px-6 rounded">About Us</button>
                 </div>
             </div>
             <div className="flex-1">
@@ -49,19 +49,35 @@ export default function Home() {
             </div>
             <div className="w-full flex flex-row flex-wrap p-20 justify-center">
                 <div className="m-5 p-5 bg-blue flex flex-col content-center h-64 w-64 text-center">
-                    <h3 className="mb-5 text-2xl text-blue-500 font-semibold">Mobile Development</h3>
+                    <h3 className="mb-5 text-2xl text-blue-800 font-semibold">Mobile Development</h3>
                     <p>We utilize cutting edge technology to build world class android and ios apps</p>
                 </div>
                <div className="m-5 p-5 bg-blue flex flex-col content-center h-64 w-64 text-center">
-                    <h3 className="mb-5 text-2xl text-blue-500 font-semibold">Mobile Development</h3>
+                    <h3 className="mb-5 text-2xl text-blue-800 font-semibold">Mobile Development</h3>
                     <p>We utilize cutting edge technology to build world class android and ios apps</p>
                 </div>
                 <div className="m-5 p-5 bg-blue flex flex-col content-center h-64 w-64 text-center">
-                    <h3 className="mb-5 text-2xl text-blue-500 font-semibold">Mobile Development</h3>
+                    <h3 className="mb-5 text-2xl text-blue-800 font-semibold">Mobile Development</h3>
                     <p>We utilize cutting edge technology to build world class android and ios apps</p>
                 </div>
                 <div className="m-5 p-5 bg-blue flex flex-col content-center h-64 w-64 text-center">
-                    <h3 className="mb-5 text-2xl text-blue-500 font-semibold">Mobile Development</h3>
+                    <h3 className="mb-5 text-2xl text-blue-800 font-semibold">Mobile Development</h3>
+                    <p>We utilize cutting edge technology to build world class android and ios apps</p>
+                </div>
+                <div className="m-5 p-5 bg-blue flex flex-col content-center h-64 w-64 text-center">
+                    <h3 className="mb-5 text-2xl text-blue-800 font-semibold">Mobile Development</h3>
+                    <p>We utilize cutting edge technology to build world class android and ios apps</p>
+                </div>
+               <div className="m-5 p-5 bg-blue flex flex-col content-center h-64 w-64 text-center">
+                    <h3 className="mb-5 text-2xl text-blue-800 font-semibold">Mobile Development</h3>
+                    <p>We utilize cutting edge technology to build world class android and ios apps</p>
+                </div>
+                <div className="m-5 p-5 bg-blue flex flex-col content-center h-64 w-64 text-center">
+                    <h3 className="mb-5 text-2xl text-blue-800 font-semibold">Mobile Development</h3>
+                    <p>We utilize cutting edge technology to build world class android and ios apps</p>
+                </div>
+                <div className="m-5 p-5 bg-blue flex flex-col content-center h-64 w-64 text-center">
+                    <h3 className="mb-5 text-2xl text-blue-800 font-semibold">Mobile Development</h3>
                     <p>We utilize cutting edge technology to build world class android and ios apps</p>
                 </div>
             </div>
@@ -76,17 +92,14 @@ export default function Home() {
                     <p>We utilize cutting edge technology to build world class android and ios apps</p>
                 </div>
                 <div className="m-5 p-5 bg-blue flex flex-col content-center h-64 w-64 text-center">
-                    <h3 className="mb-5 text-2xl text-blue-500 font-semibold">Mobile Development</h3>
+                    <h3 className="mb-5 text-2xl text-blue-800 font-semibold">Mobile Development</h3>
                     <p>We utilize cutting edge technology to build world class android and ios apps</p>
                 </div>
                 <div className="m-5 p-5 bg-blue flex flex-col content-center h-64 w-64 text-center">
-                    <h3 className="mb-5 text-2xl text-blue-500 font-semibold">Mobile Development</h3>
+                    <h3 className="mb-5 text-2xl text-blue-800 font-semibold">Mobile Development</h3>
                     <p>We utilize cutting edge technology to build world class android and ios apps</p>
                 </div>
-                <div className="m-5 p-5 bg-blue flex flex-col content-center h-64 w-64 text-center">
-                    <h3 className="mb-5 text-2xl text-blue-500 font-semibold">Mobile Development</h3>
-                    <p>We utilize cutting edge technology to build world class android and ios apps</p>
-                </div>
+                
             </div>
         </div>
 
@@ -95,10 +108,16 @@ export default function Home() {
             <div>
                 <h1>Product Showcase</h1>
             </div>
-            <div>
-                <div>
-                    <h3>Sonic.io</h3>
-                    <p></p>
+            <div className="w-full ">
+                <div className="w-full bg-blue-800 flex flex-row">
+                    <div className="p-20">
+                        <h3 className="mb-10 text-4xl text-white font-bold">Sonic.io</h3>
+                         <p className="mb-10 text-1xl text-white"> Fast and easy invoicing for entrepreneu4rs and freelanceras in nigeris</p>
+                         <button>Explore</button>
+                    </div>
+                    <div>
+                        <p>Insert image here</p>
+                    </div>
                 </div>
             </div>
         </div>
@@ -106,8 +125,8 @@ export default function Home() {
 
       <footer  className="w-full" >
       <div className="w-full flex flex-row">
-        <div className="w-52 m-5">
-          <img src="/dd.jpg" alt="logo" className="w-30 h-20"/>
+        <div className="w-96 m-10 mt-5">
+          <img src="/dd.jpg" alt="logo" className="w-52 h-52"/>
         </div>
         <div className="w-52 m-5">
             <h3 className="text-1xl font-bold">Links</h3>
